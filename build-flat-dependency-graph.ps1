4c9f2c398bb60dfe474b41e52a04ecf06a4eb6a3
Bas Lijten <bas.lijten@achmea.nl>
2018-08-15 17:11:23 +0200

    added output

---
+++ build-flat-dependency-graph.ps1
@@ -0,0 +1,143 @@
+﻿#Param(
+#    [parameter(Mandatory=$true, HelpMessage="Version"][ValidateNotNull()][String] $Provider,
+#    [parameter(Mandatory=$true, HelpMessage="Version"][ValidateNotNull()][String] $Version
+#)
+
+# check if packagesource exists
+#Register-PackageSource -Name "sitecore-myget" -Location "https://sitecore.myget.org/F/sc-packages/api/" -ProviderName "nuget"
+#Register-PackageSource -Name "nuget2" -Location "http://www.nuget.org/api/v2/" -ProviderName "nuget"
+
+$sc_version = "9.0.171219"; #Sitecore 9.0.1
+$file = "<path>\flat-dependency-graph-$sc_version.txt"
+
+function Create-Dependency
+{
+    Param(
+        [parameter(Mandatory=$true)][ValidateNotNull()]$Package,
+        [parameter(Mandatory=$true)][ValidateNotNull()]$DependencyName,
+        [parameter(Mandatory=$true)][ValidateNotNull()]$DependencyVersion
+    )
+
+    $dependency = "`"$($Package.Name) $($Package.Version)`" -> `"$($DependencyName) $($DependencyVersion)`""
+    return $dependency
+}
+
+function Check-If-Installed
+{
+Param(
+    [parameter(Mandatory=$true, HelpMessage="Version")][ValidateNotNull()][String] $Name,
+    [parameter(Mandatory=$true, HelpMessage="Version")][ValidateNotNull()][String] $Version    
+    )
+    $pkg = $null
+    $pkg = Get-package -Name $Name -RequiredVersion $Version -ErrorAction SilentlyContinue    
+    return $pkg
+}
+
+function Write-Dependencies {
+    Param(
+    [parameter(Mandatory=$true, HelpMessage="Version")][ValidateNotNull()] $Package    
+    )
+    Write-Host "Dependencies of $($Package.Name) - $($Package.Dependencies.Count)"
+
+    $nameversion = $null;
+    foreach($dependency in $Package.Dependencies)
+    {        
+        $nameversion = $dependency.Split(":")[1].Split("/")
+        $name = $name = $nameversion[0]
+        $version = $nameversion[1].Replace("[", "").Replace("]", "")
+
+        #recursive call to Install-Package
+        $dep = Create-Dependency -Package $Package -DependencyName $name -DependencyVersion $version        
+        Install-Nuget-Package-Dependency-From-Source -Name $name -Version $version -Source "bla"
+        Add-Content $file $dep       
+    }    
+}
+
+function Install-Nuget-Package-Dependency-From-Source
+{
+    Param(
+    [parameter(Mandatory=$true, HelpMessage="Version")][ValidateNotNull()][String] $Name,
+    [parameter(Mandatory=$true, HelpMessage="Version")][ValidateNotNull()][String] $Version,    
+    [parameter(Mandatory=$true, HelpMessage="Version")][ValidateNotNull()][String] $Source  
+    )    
+
+    #Check if installed - using Get-Package
+    $pkg = Check-If-Installed -Name $Name -Version $Version        
+    if($pkg -ne $null)
+    {
+        Write-Host "$Name already installed - next dependency"
+        return;
+    }
+
+    # If not installed, try to find the package
+    if($pkg -eq $null)
+    {                
+        $pkg = Find-Package -Name $Name -RequiredVersion $Version -Source "sitecore-myget" -ErrorAction SilentlyContinue
+        #if not existent, fallback to nuget.org
+        if($pkg -eq $null)
+        {
+            $pkg = Find-Package -Name $Name -RequiredVersion $Version -Source "nuget2" -ErrorAction SilentlyContinue
+        }                
+    }    
+
+    #if package found (in feed or in local storage)
+    if($pkg -ne $null)
+    {     
+        #after installing dependencies, install package
+        Install-Package -Name $pkg.Name -RequiredVersion $pkg.Version -Source $pkg.Source -Force -Confirm:$false -ForceBootstrap:$true -SkipDependencies        
+    }
+}
+
+function Install-Nuget-Package-From-Source
+{
+    Param(
+    [parameter(Mandatory=$true, HelpMessage="Version")][ValidateNotNull()][String] $Name,
+    [parameter(Mandatory=$true, HelpMessage="Version")][ValidateNotNull()][String] $Version,    
+    [parameter(Mandatory=$true, HelpMessage="Version")][ValidateNotNull()][String] $Source  
+    )
+    $isInstalled = $false
+
+    #Check if installed - using Get-Package
+    $pkg = Check-If-Installed -Name $Name -Version $Version        
+    # If not installed, try to find the package
+    if($pkg -eq $null)
+    {                
+        $pkg = Find-Package -Name $Name -RequiredVersion $Version -Source "sitecore-myget" -ErrorAction SilentlyContinue
+        #if not existent, fallback to nuget.org
+        if($pkg -eq $null)
+        {
+            $pkg = Find-Package -Name $Name -RequiredVersion $Version -Source "nuget2" -ErrorAction SilentlyContinue
+        }                
+    }
+    else {$isInstalled = $True}
+
+    #if package found (in feed or in local storage)
+    if($pkg -ne $null)
+    {
+        Write-Host "$($pkg.Name) found in $($pkg.Source) - now checking dependencies"
+        #install dependencies
+        if($pkg.Dependencies.Count -gt 0)
+        {
+            Write-Dependencies -Package $pkg                                    
+        }
+        #after installing dependencies, install package
+        if(!$isInstalled) {
+            Install-Package -Name $pkg.Name -RequiredVersion $pkg.Version -Source $pkg.Source -Force -Confirm:$false -ForceBootstrap:$true
+        }
+    }
+}
+
+# $packages = Find-Package -Source "sitecore-myget" -AllVersions 
+$packages901 = $packages | where {$_.Version -eq "$sc_version" } 
+$packages901WithReferences = $packages901 | where {$_.Name -notlike "*NoReferences"}
+$packages901NoReferences = $packages901 | where {$_.Name -like "*NoReferences"}
+
+foreach($package in $packages901WithReferences)
+{    
+    Write-Host "installing $($package.Name)"     
+
+    Install-Nuget-Package-From-Source -Name $package.Name -Version $package.Version -Source $package.Source
+
+    Write-Host "Installed $($package.Name)"
+    Write-Host ""
+}
---
+++ push-to-feed.ps1
@@ -0,0 +1,129 @@
+﻿### Script based on script by Rene van Osnabrugge "https://roadtoalm.com/author/osnabrugge/"
+### original can be found here: https://roadtoalm.com/2017/01/16/programmatically-promote-your-package-quality-with-release-views-in-vsts/
+### I just added some logic to iterate through all my local packages to upload them to a feed and promote them to a specific view.
+
+
+function Create-VSTSPackagemanagementUrl {
+    Param(
+        [parameter(Mandatory=$true)][string] $Subscription        
+    )
+    
+    $baseurl = "pkgs.visualstudio.com/_apis/packaging"    
+    
+    [string]$url = "https://$($Subscription).$($baseurl)"
+    return $url
+}
+
+function Push-ToNuget {
+    Param(
+    [parameter(Mandatory=$true)][string] $PackagePath,
+    [parameter(Mandatory=$true)][string] $Feed,
+    [parameter(Mandatory=$false)][string] $View = ""
+    )
+
+    if(![String]::IsNullOrEmpty($View)) {
+        $Feed = $Feed + "@" + $View
+    }
+
+    $nugetToCall = "C:\git\get-sitecore-packages\CredentialProviderBundle\nuget.exe"
+     &$nugetToCall push -Source $Feed -ApiKey VSTS "$PackagePath"
+
+}
+
+<#
+.Synopsis
+Creates either a Basic Authentication token or a Bearer token depending on where the method is called from VSTS. 
+When you send a Personal Access Token that you generate in VSTS it uses this one. Within the VSTS pipeline it uses env:System_AccessToken 
+#>
+function New-VSTSAuthenticationToken
+{
+    [CmdletBinding()]
+    [OutputType([object])]
+         
+    $accesstoken = "";
+    if([string]::IsNullOrEmpty($env:System_AccessToken)) 
+    {
+        if([string]::IsNullOrEmpty($env:PersonalAccessToken))
+        {
+            throw "No token provided. Use either env:PersonalAccessToken for Localruns or use in VSTS Build/Release (System_AccessToken)"
+        } 
+        Write-Debug $($env:PersonalAccessToken)
+        $userpass = ":$($env:PersonalAccessToken)"
+        $encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($userpass))
+        $accesstoken = "Basic $encodedCreds"
+    }
+    else 
+    {
+        $accesstoken = "Bearer $env:System_AccessToken"
+    }
+
+    return $accesstoken;
+}
+
+function Set-PackageQuality
+{
+    [CmdletBinding()]
+    [OutputType([object])]
+    param
+    (
+        [string] $feedType="nuget",
+        [string] $feedName="",
+        [string] $packageId="",
+        [string] $packageVersion="",
+        [string] $packageQuality=""
+        
+    )
+
+    $token = New-VSTSAuthenticationToken    
+    #API URL is slightly different for npm vs. nuget...
+    switch($feedType)
+    {
+        "npm" { $releaseViewURL = "$basepackageurl/feeds/$feedName/npm/$packageId/versions/$($packageVersion)?api-version=5.0-preview.1" }
+        "nuget" { $releaseViewURL = "$basepackageurl/feeds/$feedName/nuget/packages/$packageId/versions/$($packageVersion)?api-version=3.0-preview.1" }
+        default { $releaseViewURL = "$basepackageurl/feeds/$feedName/nuget/packages/$packageId/versions/$($packageVersion)?api-version=3.0-preview.1" }
+    }
+    
+     $json = @{
+        views = @{
+            op = "add"
+            path = "/views/-"
+            value = "$packageQuality"
+        }
+    }
+
+    $response = Invoke-RestMethod -Uri $releaseViewURL -Headers @{Authorization = $token}   -ContentType "application/json" -Method Patch -Body (ConvertTo-Json $json)
+    return $response
+}
+ 
+
+
+$feed = "xxxx.Sitecore"
+$view = ""
+$view = "Release-9.0-update-1"
+$subscription = "xxx-xxxxxxx-xxx"
+
+$basepackageurl = Create-VSTSPackagemanagementUrl -Subscription $subscription
+
+#?? needed or just needed to register a source?
+#
+#$fullurl = Create-VSTSPackagemanagementUrl -Subscription $subscription -Feed $feed -View $view
+
+$path = "C:\Program Files\PackageManagement\NuGet\Packages\"
+
+$sc_version = "9.0.180604";
+$versionstring = ".NoReferences.$($sc_version).nupkg"
+
+$nugetpackages = Get-ChildItem -Path $path -Include "*$versionstring" -Recurse
+Write-Host "$($nugetpackages.Count) packages found"
+$pkg = $null
+
+foreach($pkg in $nugetpackages)
+{    
+    $nugetpackage = $pkg.Name.Replace(".$sc_version.nupkg", "")
+
+    Push-ToNuget -PackagePath $pkg -Feed $feed
+    Set-PackageQuality -feedName $feed -packageId $nugetpackage -packageVersion $sc_version -packageQuality $view
+}
+
+
+
